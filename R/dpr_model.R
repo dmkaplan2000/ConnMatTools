37ac4a142f016b76475b91cd6fc23d2f9412dd38
dk1f0c9 <dk1f0c9@b5893b7e-e426-0410-b8a3-95cf411c6d53>
2013-07-08 19:48:17 +0000

    Adding DPR model.
    git-svn-id: https://forge.ifremer.fr/svn/dmk/trunk/docs/projects/ConnMatTools@5102 b5893b7e-e426-0410-b8a3-95cf411c6d53

--- R/dpr_model.R
+++ R/dpr_model.R
@@ -1,14 +1,19 @@
 # Implements code in the following references:
 # 
-# @references Grüss, A., Kaplan, D. M., and Lett, C. 2012. Estimating local
-#   settler–recruit relationship parameters for complex spatially explicit
+# @references Grüss, A., Kaplan, D. M., and Lett, C. 2012. Estimating local 
+#   settler–recruit relationship parameters for complex spatially explicit 
 #   models. Fisheries Research, 127–128: 34–39.
-# @references Kaplan, D. M., Botsford, L. W., and Jorgensen, S. 2006. Dispersal
-#   per recruit: An efficient method for assessing sustainability in marine
+# @references Kaplan, D. M., Botsford, L. W., and Jorgensen, S. 2006. Dispersal 
+#   per recruit: An efficient method for assessing sustainability in marine 
 #   reserve networks. Ecological Applications, 16: 2248–2263.
-# @references White, J. W. 2010. Adapting the steepness parameter from
-#   stock-recruit curves for use in spatially explicit models. Fisheries
+# @references White, J. W. 2010. Adapting the steepness parameter from 
+#   stock-recruit curves for use in spatially explicit models. Fisheries 
 #   Research, 102: 330–334.
+# @references Grüss A, Kaplan DM, Hart DR (2011) Relative Impacts of Adult
+#   Movement, Larval Dispersal and Harvester Movement on the Effectiveness of
+#   Reserve Networks. PLoS ONE 6:e19960
+# @references Beverton RJH, Holt SJ (1957) On the dynamics of exploited fish
+#   populations. H.M.S.O., London. 533 pp.
 NULL
 
 #' Correction slope of settler-recruit relationship
@@ -58,16 +63,88 @@ settler.recruit.slope.correction <- function(slope,conn.mat,natural.LEP=1,
   return(slope/Mod(v))
 }
 
+#' Beverton-Holt settler-recruit relationship
+#' 
+#' Calculates recruitment based on the settler-recruit relationship from 
+#' Beverton & Holt (1957): \code{ slope * settlers / (1+slope*settlers/Rmax) }
+#' 
+#' \code{slope} and \code{Rmax} can both either be scalars or vectors of the
+#' same length as \code{S}.
+#' 
+#' @param S a vector of settlement values, 1 for each site.
+#' @param slope slope at the origin of the settler-recruit relationship.  Can be
+#'   a vector of same length as \code{S}.
+#' @param Rmax maximum recruitment value.
+#'   
+#' @return A vector of recruitment values.
+#'   
+#' @references Beverton RJH, Holt SJ (1957) On the dynamics of exploited fish 
+#'   populations. H.M.S.O., London. 533 pp.
+#'   
+#' @author David Kaplan \email{dmkaplan2000@@gmail.com}
+#' @export
 beverton.holt <- function(S,slope,Rmax) {
   return( (slope * S) / (1 + slope * S / Rmax ) )
 }
 
+#' Hockey-stick settler-recruit relationship
+#' 
+#' Calculates recruitment based on a settler-recruit relationship that increases
+#' linearly until it reaches a maximum values.
+#' 
+#' \code{slope} and \code{Rmax} can both either be scalars or vectors of the 
+#' same length as \code{S}.
+#' 
+#' @param S a vector of settlement values, 1 for each site.
+#' @param slope slope at the origin of the settler-recruit relationship.  Can be
+#'   a vector of same length as \code{S}.
+#' @param Rmax maximum recruitment value.
+#'   
+#' @return A vector of recruitment values.
+#'   
+#' @references Kaplan, D. M., Botsford, L. W., and Jorgensen, S. 2006. Dispersal
+#'   per recruit: An efficient method for assessing sustainability in marine 
+#'   reserve networks. Ecological Applications, 16: 2248–2263.
+#'   
+#' @author David Kaplan \email{dmkaplan2000@@gmail.com}
+#' @export
 hockey.stick <- function(S,slope,Rmax) {
   R = slope * S
   R[ R>Rmax] = Rmax
   return( R )
 }
 
+#' Population dynamics model based on lifetime-egg-production
+#' 
+#' This function implements the marine population dynamics model described in
+#' Kaplan et al. (2006).  This model is most appropriate for examining
+#' equilibrium dynamics of age-structured populations or temporal dynamics of
+#' semelparous populations.
+#' 
+#' @param LEP a vector of lifetime-egg-production (LEP; also known as
+#'   eggs-per-recruit (EPR)) for each site.
+#' @param conn.mat a square connectivity matrix.  \code{dim(conn.mat) =
+#'   rep(length(LEP),2)}
+#' @param recruits0 a vector of initial recruitment values for each site.
+#' @param timesteps a vector of timesteps at which to record egg production,
+#'   settlement and recruitment.
+#' @param settler.recruit.func a function to calculate recruitment from the
+#'   number of settlers at each site.  Defaults to \code{\link{beverton.holt}}.
+#' @param \dots additional arguments to settler.recruit.func.
+#'   
+#' @return A list with the following elements:
+#' \item{eggs}{egg production for the timesteps in \code{timesteps}}
+#'
+#' \item{settlers}{Similar for settlement}
+#'   
+#' \item{recruits}{Similar for recruitment}
+#'   
+#' @references Kaplan, D. M., Botsford, L. W., and Jorgensen, S. 2006. Dispersal
+#'   per recruit: An efficient method for assessing sustainability in marine 
+#'   reserve networks. Ecological Applications, 16: 2248–2263.
+#'   
+#' @author David Kaplan \email{dmkaplan2000@@gmail.com}
+#' @export
 dispersal.per.recruit.model <- 
   function(LEP,conn.mat,
            recruits0=matrix(tryCatch(Rmax,finally=1),nrow=dim(conn.mat)[2],ncol=1),
