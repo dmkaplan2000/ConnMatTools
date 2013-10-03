0be58e68e2fd9ecb93162726f0b9d83529d75bb0
dk1f0c9 <dk1f0c9@b5893b7e-e426-0410-b8a3-95cf411c6d53>
2013-10-03 13:53:49 +0000

    Improving selfRecruitment function so that it takes egg production into account.
    git-svn-id: https://forge.ifremer.fr/svn/dmk/trunk/docs/projects/ConnMatTools@5263 b5893b7e-e426-0410-b8a3-95cf411c6d53

--- R/retentionStats.R
+++ R/retentionStats.R
@@ -29,16 +29,22 @@ relativeLocalRetention <- function(conn.mat) {
 
 #' Self recruitment of a connectivity matrix
 #' 
-#' Self recruitment is defined as the diagonal elements of the
-#' connectivity matrix divided by the sum of the corresponding row of the
-#' connectivity matrix.
+#' If egg production is uniform over sites, then self recruitment is defined as 
+#' the diagonal elements of the connectivity matrix divided by the sum of the 
+#' corresponding row of the connectivity matrix.  If not, then the elements of 
+#' the dispersal matrix must be weighted by the number of eggs produced.
 #' 
 #' @param conn.mat A square connectivity matrix.
+#' @param eggs A vector of egg production values for each site.  Defaults to
+#'   \code{NULL}, equivalent to assuming all sites have equal egg production.
 #'   
 #' @author David M. Kaplan \email{dmkaplan2000@@gmail.com}
 #' @example tests/test.retentionStats.R
 #' @export
-selfRecruitment <- function(conn.mat) {
+selfRecruitment <- function(conn.mat,eggs=NULL) {
+  if !(is.null(eggs))
+    conn.mat = conn.mat %*% diag(eggs)
+  
   ss = apply(conn.mat,1,sum)
   ss[ ss == 0 ] = 1
   localRetention(conn.mat) / ss  
