68b856eb9d563949ecde0decfd21b366f7034a5b
David M. Kaplan <dmkaplan2000@gmail.com>
2014-06-04 14:52:56 +0200

    Small fixes to remove errors when running test scripts for optimal split.

--- R/ConnMatTools.R
+++ R/ConnMatTools.R
@@ -27,11 +27,11 @@
 #' 
 #' @references Jacobi, M. N., and Jonsson, P. R. 2011. Optimal networks of 
 #'   nature reserves can be found through eigenvalue perturbation theory of the 
-#'   connectivity matrix. Ecological Applications, 21: 1861–1870.
-#' @references Jacobi, M. N., André, C., Döös, K., and Jonsson,
+#'   connectivity matrix. Ecological Applications, 21: 1861-1870.
+#' @references Jacobi, M. N., Andre, C., Doos, K., and Jonsson,
 #' P. R. 2012. Identification of subpopulations from connectivity
 #' matrices. Ecography, 35: 1004-1016.
-#' @references Grüss, A., Kaplan, D. M., and Lett, C. 2012. Estimating local 
+#' @references Gruss, A., Kaplan, D. M., and Lett, C. 2012. Estimating local 
 #'   settler-recruit relationship parameters for complex spatially explicit 
 #'   models. Fisheries Research, 127-128: 34-39.
 #' @references Kaplan, D. M., Botsford, L. W., and Jorgensen, S. 2006. Dispersal 
@@ -40,7 +40,7 @@
 #' @references White, J. W. 2010. Adapting the steepness parameter from 
 #'   stock-recruit curves for use in spatially explicit models. Fisheries 
 #'   Research, 102: 330-334.
-#' @references Grüss A, Kaplan DM, Hart DR (2011) Relative Impacts of Adult
+#' @references Gruss A, Kaplan DM, Hart DR (2011) Relative Impacts of Adult
 #'   Movement, Larval Dispersal and Harvester Movement on the Effectiveness of
 #'   Reserve Networks. PLoS ONE 6:e19960
 #' @references Beverton RJH, Holt SJ (1957) On the dynamics of exploited fish
